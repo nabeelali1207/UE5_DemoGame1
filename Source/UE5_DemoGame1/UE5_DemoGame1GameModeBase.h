059c2f073656f33d1e82512174eb7bda6507c50d
Nabeel Ali <nabeel1207ali@gmail.com>
2023-06-29 15:00:12 +0500

    Initial commit

---
+++ Source/UE5_DemoGame1/UE5_DemoGame1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_DemoGame1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_DEMOGAME1_API AUE5_DemoGame1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
